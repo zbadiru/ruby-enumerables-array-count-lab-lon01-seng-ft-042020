340d41f8f28a20baeb591260bf60ffaa20c4ca95
Zakari Badiru <zbadiru@live.co.uk>
2020-04-04 19:07:19 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,8 +1,7 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
-array.count do 
-end
-total 
+array.count do |element|
+  element == String 
 end
 
 def count_empty_strings(array)
